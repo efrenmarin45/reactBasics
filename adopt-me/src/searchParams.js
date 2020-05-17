e8df79afe8499e90639b16413f9a685c83a7c7a8
Efren_Marin <efren45marin@gmail.com>
2020-05-16 21:14:46 -0700

    Created dropdown component to better manage app.

--- adopt-me/src/searchParams.js
+++ adopt-me/src/searchParams.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+//Hooks use current state (location) and the updated state (setLocation)
+//All hooks will begin with the word "use"
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA"); //Hooks use current state (location) and the updated state (setLocation)
-  //All hooks will begin with the word "use"
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("All");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal");
 
   return (
     <div className="search-params">
@@ -16,6 +24,38 @@ const SearchParams = () => {
             placeholder="location"
             onChange={event => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={event => setAnimal(event.target.value)}
+            onBlur={event => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={event => setBreed(event.target.value)}
+            onBlur={event => setBreed(event.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedString => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+            ;
+          </select>
         </label>
         <button>Submit</button>
       </form>
