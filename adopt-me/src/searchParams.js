c58c75b8785276be2d00c7c7eda4dc6f9a1b4b65
Efren_Marin <efren45marin@gmail.com>
2020-05-16 19:52:05 -0700

    Made first Hook. Added comments.

--- adopt-me/src/searchParams.js
+++ adopt-me/src/searchParams.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA"); //Hooks use current state (location) and the updated state (setLocation)
+  //All hooks will begin with the word "use"
 
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={event => setLocation(event.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
