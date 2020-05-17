3d43eee7ccb2305cf5f2c52d0758d5fdf5ac25c6
Efren_Marin <efren45marin@gmail.com>
2020-05-16 19:32:04 -0700

    Added new file, input field and submit button in JSX

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,39 +1,12 @@
 import React from "react"; //importing React from our npm installation of react
 import { render } from "react-dom"; //We only need render from the npm installation of react-dom
-import Pet from "./Pet"; //Since we have the Pet component in another file, we are importing it here
+import SearchParams from "./searchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Oreo",
-  //     animal: "Cat",
-  //     breed: "Fluffy"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Roxy",
-  //     animal: "Dog",
-  //     breed: "Pitbull"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Lobo",
-  //     animal: "Dog",
-  //     breed: "German Sheppard"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Sparky",
-  //     animal: "Dog",
-  //     breed: "Boxer"
-  //   })
-  // ]);
-
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Oreo" animal="Cat" breed="Fluffy" />
-      <Pet name="Roxy" animal="Dog" breed="Pitbull" />
-      <Pet name="Lobo" animal="Dog" breed="German Sheppard" />
-      <Pet name="Sparky" animal="Dog" breed="Boxer" />
+      <SearchParams />
     </div>
   );
 };
