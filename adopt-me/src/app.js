3d2e17d89b34bd2bba61557382630770da12d2ec
Efren_Marin <efren45marin@gmail.com>
2020-06-16 23:19:29 -0700

    Worked on app.js file to include a link to the words, Adopt Me. Now when clicked, it will take the user to the home screen.

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,13 +1,15 @@
 import React from "react"; //importing React from our npm installation of react
 import { render } from "react-dom"; //We only need render from the npm installation of react-dom
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./searchParams";
 import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
       <Router>
         <SearchParams path="/" />
         <Details path="/details/:id" />
