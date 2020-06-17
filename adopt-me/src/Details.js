300437e1866d593b97cac5d5213ebb653c94e120
Efren_Marin <efren45marin@gmail.com>
2020-06-16 23:08:14 -0700

    Added a separate file to check Reach Router. Added Reach Router on app.js.

---
+++ adopt-me/src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = () => {
+  return <h1>'ello Govnah!</h1>;
+};
+
+export default Details;
--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,12 +1,17 @@
 import React from "react"; //importing React from our npm installation of react
 import { render } from "react-dom"; //We only need render from the npm installation of react-dom
+import { Router } from "@reach/router";
 import SearchParams from "./searchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
